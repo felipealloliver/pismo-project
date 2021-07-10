27ad62969c57a9c38f55d57deda63e1e15a2fce4
Felipe Allan <orto.f@hotmail.com>
2021-07-10 20:14:53 -0300

    Configuração Inicial. docker-compose e postgresql

---
+++ src/main/java/com/pismo/project/PismoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pismo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PismoProjectApplication.class, args);
+    }
+
+}
